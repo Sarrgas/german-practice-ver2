d28f9bb0ee81e89f4e3727ac92edfc80d61e3554
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2019-08-12 14:08:10 +0200

    More verbs

--- src/data/verbs.js
+++ src/data/verbs.js
@@ -32,15 +32,83 @@ const verbs = [
         ihr: 'esst',
         sie: 'essen'
     },
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
+    {
+        id: 4,
+        german: 'Verstehen',
+        swedish: 'Förstå',
+        ich: 'verstehe',
+        du: 'verstehst',
+        ersiees: 'versteht',
+        wir: 'verstehen',
+        ihr: 'versteht',
+        sie: 'verstehen'
+    },
+    {
+        id: 5,
+        german: 'Fressen',
+        swedish: 'Äta',
+        ich: 'fresse',
+        du: 'frisst',
+        ersiees: 'frisst',
+        wir: 'fressen',
+        ihr: 'fresst',
+        sie: 'fressen'
+    },
+    {
+        id: 6,
+        german: 'Kommen',
+        swedish: 'Komma',
+        ich: 'komme',
+        du: 'kommst',
+        ersiees: 'kommt',
+        wir: 'kommen',
+        ihr: 'kommt',
+        sie: 'kommen'
+    },
+    {
+        id: 7,
+        german: 'Heißen',
+        swedish: 'Heta',
+        ich: 'heiße',
+        du: 'heißt',
+        ersiees: 'heißt',
+        wir: 'heißen',
+        ihr: 'heißt',
+        sie: 'heißen'
+    },
+    {
+        id: 8,
+        german: 'Sprechen',
+        swedish: 'Prata',
+        ich: 'spreche',
+        du: 'sprichst',
+        ersiees: 'spricht',
+        wir: 'sprechen',
+        ihr: 'sprecht',
+        sie: 'sprechen'
+    },
+    {
+        id: 9,
+        german: 'Lesen',
+        swedish: 'Läsa',
+        ich: 'lese',
+        du: 'liest',
+        ersiees: 'liest',
+        wir: 'lesen',
+        ihr: 'lest',
+        sie: 'lesen'
+    },
+    {
+        id: 10,
+        german: 'Trinken',
+        swedish: 'Dricka',
+        ich: 'trinke',
+        du: 'trinkst',
+        ersiees: 'trinkt',
+        wir: 'trinken',
+        ihr: 'trinkt',
+        sie: 'trinken'   
+    },
     
 ];
 
