5fdc7026fce62a2ac7fe8bd9715891b4ce86b915
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2019-08-12 15:10:16 +0200

    Oops missed a file

---
+++ src/Notes/NotePage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="note">
+            <h3>Ackusativ</h3>
+            <p>
+                Maskulina ord (med artikeln "der") ändras om de efterföljer ett verb. <br />
+                "Der" ändras då till "den", och "ein" ändras då till "einen".<br />
+                Exempelvis:<br />
+                Ich verstehe <b>den</b> Hund. <br />
+                Ich habe <b>einen</b> Hund.
+            </p>
+        </div>
+        <div class="note">
+            <h3>Frågor</h3>
+            <p>
+                Frågor i tyska fungerar som på svenska, alltså att orden byter plats. <br />
+                Exempel: <br />
+                Du hast bier - Du har öl. <br />
+                Hast du bier? - Har du öl?
+            </p>
+        </div>
+        <div class="note">
+            <h3>Hungrig/törstig</h3>
+            <p>
+                Kommer inte på vad det korrekta ordet för denna grammatik är, men tyskan gör som spanskan. <br />
+                Om vi på svenska säger "Jag <b>är</b> hungrig" så säger tyskan "Jag <b>har</b> hunger". <br />
+                Exempelvis: <br />
+                Ich habe Hunger - Jag har hunger <br />
+                Ich habe Durst - Jag har törst <br />
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotePage'
+}
+</script>
+
+<style scoped>
+.note{
+    text-align: left;
+    border: 2px solid black;
+    border-radius: 5px;
+    padding: 10px;
+    width: 500px;
+    margin: 5px;
+}
+</style>
