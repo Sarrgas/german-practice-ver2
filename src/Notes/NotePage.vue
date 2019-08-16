29e636bd90c91e04bdbbe9aaf4c70f888363271f
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2019-08-16 09:14:07 +0200

    Moved notes to js data file

--- src/Notes/NotePage.vue
+++ src/Notes/NotePage.vue
@@ -1,77 +1,22 @@
 <template>
     <div id="content">
-        <div class="note">
-            <h3>Ackusativ</h3>
-            <p>
-                Maskulina ord (med artikeln "der") ändras om de efterföljer ett verb. <br />
-                "Der" ändras då till "den", och "ein" ändras då till "einen".<br />
-                Exempelvis:<br />
-                Ich verstehe <b>den</b> Hund. <br />
-                Ich habe <b>einen</b> Hund.
-            </p>
-        </div>
-        <div class="note">
-            <h3>Frågor</h3>
-            <p>
-                Frågor i tyska fungerar som på svenska, alltså att orden byter plats. <br />
-                Exempel: <br />
-                Du hast bier - Du har öl. <br />
-                Hast du bier? - Har du öl?
-            </p>
-        </div>
-        <div class="note">
-            <h3>Hungrig/törstig</h3>
-            <p>
-                Kommer inte på vad det korrekta ordet för denna grammatik är, men tyskan gör som spanskan. <br />
-                Om vi på svenska säger "Jag <b>är</b> hungrig" så säger tyskan "Jag <b>har</b> hunger". <br />
-                Exempelvis: <br />
-                Ich habe Hunger - Jag har hunger <br />
-                Ich habe Durst - Jag har törst <br />
-            </p>
-        </div>
-        <div class="note">
-            <h3>Plural</h3>
-            <p>
-                Alla substantiv får artikeln "Die" i plural oavsett kön. Exempel: <br />
-                <b>Der</b> Hund blir <b>Die</b> Hunden <br />
-                <b>Das</b> Mädchen blir <b>Die</b> Mädchen <br />
-                <b>Die</b> Frau förblir <b>Die</b> Frauen <br />
-            </p>
-        </div>
-        <div class="note">
-            <h3>Substantiv</h3>
-            <p>
-                Alla substantiv skrivs med stor begynnelsebokstav. <br />
-                Om man exempelvis skulle skriva "En katt åt en fluga." blir det: <br />
-                Eine <b>K</b>atze frisst eine <b>F</b>liege.
-            </p>
-        </div>
-        <div class="note">
-            <h3>Ihopsatta ord</h3>
-            <p>
-                Tyskand kan göra som svenskan och sätta ihop flera ord till ett långt.
-                Så som Flaggstångsknoppsputsningsmedel osv... 
-                Korrekt artikel (der/die/das) för det nya ordet är då avsedd artikel för det <b>sista</b> ordet. Exempel: <br>
-                <b>Die</b> Orange +<br>
-                <b>Der</b> Saft = <br>
-                <b>Der</b> Orangensaft
-            </p>
-        </div>
-        <div class="note">
-            <h3>Hitta rätt artikel</h3>
-            <p>
-                Icke-levande objekt som slutar på <b>e</b> är nästan alltid feminina (Die). <br>
-                Detsamma gäller också för de flesta djur. <br>
-                <br>
-                Ord som börjar med suffixet <b>ge</b> (Exempelvis Gemüse) är oftast neutral (Das).
-            </p>
+        <div class="note" v-for="(note, i) in notes" :key="i">
+            <h3 v-html="note.title"></h3>
+            <p v-html="note.text"></p>
         </div>
     </div>
 </template>
 
 <script>
+import notes from '../data/notes.js'
+
 export default {
-    name: 'NotePage'
+    name: 'NotePage',
+    data() {
+        return {
+            notes
+        }
+    }
 }
 </script>
 
