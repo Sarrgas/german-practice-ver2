6b2f9f91de63cc528dee9e40fc1c9a067f959bd5
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2019-08-29 14:27:05 +0200

    Add new notes to store

--- src/Add/AddNote.vue
+++ src/Add/AddNote.vue
@@ -3,18 +3,36 @@
         <h3>Lägg till en ny anteckning.</h3>
         <div>
             <label for="">Titel:</label>
-            <input type="text" placeholder="Titel" >
+            <input type="text" placeholder="Titel" v-model="title">
         </div>
         <div>
             <label for="">Text:</label>
-            <textarea name="text" id="" cols="30" rows="10"></textarea>
+            <textarea name="text" id="" cols="30" rows="10" v-model="text"></textarea>
         </div>
+        <button @click="commitNewNote">Add</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'AddNote'
+    name: 'AddNote',
+    data() {
+        return {
+            title: '',
+            text: ''
+        }
+    },
+    methods: {
+        commitNewNote(){
+            const newNote = {
+                id: 999,
+                title: this.title,
+                text: this.text
+            };
+
+            this.$store.commit('addNote', newNote);
+        }
+    }
 }
 </script>
 
