f686806605fc33dd981fca0f4cf974515be3e20b
Andreas Smidelöv <sargas1@hotmail.com>
2019-09-03 21:42:46 +0200

    changed dividers to 10

--- src/Home/HomePage.vue
+++ src/Home/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <MenuOption linkTo="Nouns" divider="10" title="Substantiv" text="Översätt substantiv från Tyska till Svenska och välj rätt artikel (der, die, das)." />
-        <MenuOption linkTo="Verbs" divider="3" title="Verb" text="Böj de tyska verben för olika personer (ich, du, er/sie/es, wir, ihr, sie)."/>
-        <MenuOption linkTo="Adjectives" divider="1" title="Adjektiv" text="Översätt adjektiv från Tyska till Svenska."/>
+        <MenuOption linkTo="Verbs" divider="10" title="Verb" text="Böj de tyska verben för olika personer (ich, du, er/sie/es, wir, ihr, sie)."/>
+        <MenuOption linkTo="Adjectives" divider="10" title="Adjektiv" text="Översätt adjektiv från Tyska till Svenska."/>
     </div>
 </template>
 
